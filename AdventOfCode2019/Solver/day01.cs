4286e955cf61e01821f1e977bbc1d7e6b2bad637
Fred <6550969+MagicFred888@users.noreply.github.com>
2025-01-26 04:53:09 +0100

    Solver for day 01

---
+++ AdventOfCode2019/Solver/day01.cs
@@ -0,0 +1,30 @@
+﻿using AdventOfCode2019.Tools;
+
+namespace AdventOfCode2019.Solver;
+
+internal partial class Day01 : BaseSolver
+{
+    public override string PuzzleTitle { get; } = "The Tyranny of the Rocket Equation";
+
+    public override string GetSolution1(bool isChallenge)
+    {
+        List<long> allStage = QuickList.ListOfLong(_puzzleInput);
+        return allStage.Sum(v => ComputeFuel(v, false)).ToString();
+    }
+
+    public override string GetSolution2(bool isChallenge)
+    {
+        List<long> allStage = QuickList.ListOfLong(_puzzleInput);
+        return allStage.Sum(v => ComputeFuel(v, true)).ToString();
+    }
+
+    private static long ComputeFuel(long moduleMass, bool computeFuelForFuel)
+    {
+        long result = (moduleMass / 3) - 2;
+        if (result <= 0)
+        {
+            return 0;
+        }
+        return result + (computeFuelForFuel ? ComputeFuel(result, true) : 0);
+    }
+}
